199ffa1e4d848ab6c7170e2bb805ae392cc057d4
waynemareci <wayne@mareci.com>
2025-03-31 00:38:51 +0000

    tests 1-3,5

--- index.js
+++ index.js
@@ -2,31 +2,70 @@
 // where your node app starts
 
 // init project
-var express = require('express');
-var app = express();
+var express = require('express')
+var app = express()
+
+const getDayName = utcDay => {
+  const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+  return daysOfWeek[utcDay]
+}
+const getMonthName = utcMonth => {
+  const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul','Aug','Sep','Oct','Nov','Dec']
+  return months[utcMonth]
+}
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+var cors = require('cors')
+app.use(cors({ optionsSuccessStatus: 200 })) // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static('public'))
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
-});
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/views/index.html')
+})
 
+app.use((req, res, next) => {
+  console.log(req.method + ' ' + req.path + ' - ' + req.ip)
+  next()
+})
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
+app.get('/api/:date?', (req, res) => {
 
+  console.log('console.log says date: ' + req.params.date)
+  const timestamp = Date.parse(req.params.date)
 
+  const dayOfWeek = getDayName(new Date(req.params.date).getDay())
+  const month = getMonthName(new Date(req.params.date).getUTCMonth())
+  const dateString =
+    dayOfWeek +
+    ', ' +
+    new Date(timestamp).getUTCDate().toString().padStart(2, '0') +
+    ' ' +
+    month +
+    ' ' +
+    new Date(timestamp).getUTCFullYear() + 
+    ' ' +
+    new Date(timestamp).getUTCHours().toString().padStart(2,'0') +
+    ':' +
+    new Date(timestamp).getUTCMinutes().toString().padStart(2,'0') +
+    ':' +
+    new Date(timestamp).getUTCSeconds().toString().padStart(2,'0') +
+    " GMT"
+
+    //res.json({"unix":timestamp})
+    //res.json({ utc: dateString })
+  res.json({"unix": timestamp,utc: dateString});
+})
+
+// your first API endpoint...
+app.get('/api/hello', function (req, res) {
+  res.json({ greeting: 'hello API' })
+})
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+  console.log('Your app is listening on port ' + listener.address().port)
+})
